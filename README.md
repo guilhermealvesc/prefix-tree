ca945f290f2dd01e549365c19133ac342a4953db
guilherme.092011 <guilherme.092011@hotmail.com>
2021-05-28 16:13:41 -0300

    project boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PREFIX-TREE
+## University Work
---
+++ src/app.c
@@ -0,0 +1,15 @@
+/*
+  Integrantes:
+  Guilherme Alves Carvalho - 11921BCC016
+  João Batista de Sousa Paula - 11911BCC008
+  Pedro Leale - 11921BCC008
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./trie/trie.h"
+int main() {
+  
+
+  return 0;
+}
