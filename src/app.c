db09ab3a993f2afab9fe819ad6be4c7dd3a8cdb6
guilherme.092011 <guilherme.092011@hotmail.com>
2021-05-29 19:28:35 -0300

    insercao/busca/imprimir/autocompletar/libera/cria trie

--- src/app.c
+++ src/app.c
@@ -9,71 +9,98 @@
 #include <stdlib.h>
 #include "./trie/trie.h"
 int main() {
-
-  Trie* trie = criaTrie();
-
-  if (trie == NULL){
+  Trie *trie = criaTrie();
+  if (trie == NULL) {
     printf("Problemas para criar a arvore\n");
     return 0;
   }
 
-  FILE *arq = fopen("palavras.txt", "r");
-  if (arq == NULL)
+  FILE *arq = fopen("./test/words.txt", "r");
+  if (arq == NULL) {
+    printf("Erro a abrir o arquivo\n");
     return 0;
-//Insercao das palavras----------------------------------------------------------------
+  }
+  //Insercao das palavras----------------------------------------------------------------
 
   char palavra[50];
-  while(1){
-    if(feof(arq))
-        break;
-    fscanf(arq,"%s", palavra);
-   // printf("%s\n",palavra); apenas para teste
-    if(insereTrie(trie, palavra))
-        printf("Sucesso ao inserir %s\n",palavra);
-    else{
-        printf("Falha ao inserir %s\n",palavra);
-        break;
-        }
+  while (1) {
+    if (feof(arq))
+      break;
+    fscanf(arq, "%s", palavra);
+    insereTrie(trie, palavra);
+    // if (insereTrie(trie, palavra))
+    //   printf("Sucesso ao inserir %s\n", palavra);
+    // else {
+    //   printf("Falha ao inserir %s\n", palavra);
+    //   break;
+    // }
+  }
+  //-------------------------------------------------------------------------------------
+  
+  char op;
+  while (1) {
+    printf("1-Inserir palavra\n2-Buscar palavra\n3-Finalizar execucao\n4-Imprimir Trie\n5-ORA ORA ORA ORA ORA ORA ORA\n6-Imprimir prefixo\nQual opcao?: ");
+    op = getchar();
+    switch (op) {
+      //-------------------------------------------------------------------------------------
+    case '1':
+      printf("Qual palavra deseja inserir?\n");
+      scanf("%s", palavra);
+      if (insereTrie(trie, palavra)) {
+        printf("Sucesso na insercao de %s\n", palavra);
+      } else {
+        printf("Falha na insercao %s\n", palavra);
+      }
+      break;
+      //-------------------------------------------------------------------------------------
+    case '2':
+      printf("Qual palavra deseja buscar?\n");
+      scanf("%s", palavra);
+      if (buscaTrie(trie, palavra)) {
+        printf("Esta palavra esta na arvore\n");
+      } else {
+        printf("Esta palavra nao esta na arvore\n");
+      }
+      break;
+      //-------------------------------------------------------------------------------------
+    case '3':
+      printf("Liberando a arvore\n");
+      liberaTrie(trie);
+      printf("Fim da execucao\n");
+      return 0;
+    case '4':
+      printf("----------PRINTANDO O TRIE FILHO EXQUECE----------\n");
+      imprimeTrie(trie);
+      break;
+    case '5': 
+        printf("⣿⣿⣿⣿⣿⣿⣿⡿⡛⠟⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
+        printf("⣿⣿⣿⣿⣿⣿⠿⠨⡀⠄⠄⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
+        printf("⣿⣿⣿⣿⠿⢁⠼⠊⣱⡃⠄⠈⠹⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
+        printf("⣿⣿⡿⠛⡧⠁⡴⣦⣔⣶⣄⢠⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣤⠭⠏⠙⢿⣿\n");
+        printf("⣿⡧⠠⠠⢠⣾⣾⣟⠝⠉⠉⠻⡒⡂⠄⠙⠻⣿⣿⣿⣿⣿⡪⠘⠄⠉⡄⢹\n");
+        printf("⣿⠃⠁⢐⣷⠉⠿⠐⠑⠠⠠⠄⣈⣿⣄⣱⣠⢻⣿⣿⣿⣿⣯⠷⠈⠉⢀⣾\n");
+        printf("⣿⣴⠤⣬⣭⣴⠂⠇⡔⠚⠍⠄⠄⠁⠘⢿⣷⢈⣿⣿⣿⣿⡧⠂⣠⠄⠸⡜\n");
+        printf("⣿⣇⠄⡙⣿⣷⣭⣷⠃⣠⠄⠄⡄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣧⣁⣿⡄⠼⡿\n");
+        printf("⣿⣷⣥⣴⣿⣿⣿⣿⠷⠲⠄⢠⠄⡆⠄⠄⠄⡨⢿⣿⣿⣿⣿⣿⣎⠐⠄⠈\n");
+        printf("⣿⣿⣿⣿⣿⣿⢟⠕⠁⠈⢠⢃⢸⣿⣿⣶⡘⠑⠄⠸⣿⣿⣿⣿⣿⣦⡀⡉\n");
+        printf("⣿⣿⣿⡟⠄⠄⠄⠄⠄⠋⢀⣼⣿⣿⣿⣿⣿⣿⣿⣶⣦⣀⢟⣻⣿⣿⣿⣿\n");
+        printf("⣿⣿⣿⡆⠆⠄⠠⡀⡀⠄⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
+        printf("⣿⣿⡿⡅⠄⠄⢀⡰⠂⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
+      break;
+    case '6':
+      printf("Qual palavra deseja utilizar?\n");
+      scanf("%s", palavra);
+      autocompletarTrie(trie, palavra);
+      break;
+      //-------------------------------------------------------------------------------------
     }
-//-------------------------------------------------------------------------------------
-      //system("pause"); //windows
-      //setbuf(stdin, NULL); //linux
-      //int c = getchar(); //linux
-      //system("clear"); //linux
-      //system("cls"); //windows
-
-   char op;
-   while(1){
-    switch(op){
-        setbuf(stdin,NULL);
-        printf("1-Remover palavra\n2-Buscar palavra\n3-Finalizar execucao");
-        op = gechar();
-//-------------------------------------------------------------------------------------
-        case '1':
-            printf("Qual palavra deseja remover?\n");
-            scanf("%s",palavra);
-            if(removeTrie(trie,palavra)){
-                printf("Sucesso na remocao de %s\n",palavra);
-            } else {
-                printf("Falha na remocao %s\n",palavra);
-            } break;
-//-------------------------------------------------------------------------------------
-        case '2':
-            printf("Qual palavra deseja buscar?\n");
-            scanf("%s",palavra);
-            if(buscaTrie(trie,palavra)){
-                printf("Esta palavra esta na arvore\n");
-            } else{
-                printf("Esta palavra nao esta na arvore\n");
-            } break;
-//-------------------------------------------------------------------------------------
-        case '3':
-            printf("Liberando a arvore\n");
-            liberaTrie(trie);
-            printf("Fim da execucao\n");
-            return 0;
-//-------------------------------------------------------------------------------------
-        }
-    }
-    return 0;
+    //system("pause"); //windows
+    setbuf(stdin, NULL); //linux
+    // int c = getchar(); //linux
+    // system("clear"); //linux
+    //system("cls"); //windows
+  }
+  liberaTrie(trie);
+  free(arq);
+  return 0;
 }
--- src/trie/trie.c
+++ src/trie/trie.c
@@ -1,68 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 #include "trie.h"
 #define MAX_CHARS 26
 
+#define hashChar(c) tolower(c) - 97
+#define getChar(i) (char) (i + 97)
 struct Trie {
   struct Trie *child[MAX_CHARS];
-  bool upper;
+  bool end;
 };
 
-Trie *criaTrie() {
-  Trie *root = (Trie *) malloc(sizeof(Trie));
-  if (root) {
+Trie startNode() {
+  Trie node = (Trie) malloc(sizeof(struct Trie));
+  if (node) {
     for (size_t i = 0; i < MAX_CHARS; i++)
-      root->child[i] = NULL;
-    root->upper = false;
+      node->child[i] = NULL;
+    node->end = false;
+  }
+  return node;
+}
+
+Trie* criaTrie() {
+  Trie* root = (Trie*) malloc(sizeof(Trie));
+  if(root) {
+    *root = startNode();
+    if(!(*root)) 
+      free(root);
   }
   return root;
 }
 
-void liberaTrie(Trie *tr) {
+void freeNodes(Trie tr) {
   if (!tr)
     return;
   for (size_t i = 0; i < MAX_CHARS; i++) {
-    if(tr->child[i])
-      liberaTrie(tr->child[i]);
+    if (tr->child[i])
+      freeNodes(tr->child[i]);
   }
   free(tr);
 }
 
-
-int insereTrie(Trie* tr, char *str) {
-  if(!tr || !str) return 0;
-  char letter = *str;
-  
-  if(letter == '\0') return 1;
-
-  if(!tr->child[letter]) 
-    tr->child[letter] = (Trie *) malloc(sizeof(Trie));
-  
-  return insereTrie(tr->child[letter], str + 1);
+void liberaTrie(Trie *tr) {
+  if(!tr) return;
+  freeNodes(*tr);
+  *tr = NULL;
 }
 
-int buscaTrie(Trie* tr, char *str) {
-  if(!tr || !str) return 0;
+int insereTrie(Trie *tr, char *str) {
+  if (!tr || !str)
+    return 0;
+  Trie node = *tr;
   char letter = *str;
-
-  if(letter == '\0') return 1;
-
-  if(!tr->child[letter]) return 0;
-
-  return buscaTrie(tr->child[letter], str + 1);
+  if (letter == '\0') {
+    node->end = true;
+    return 1;
+  }
+  //if alpha, insert
+  if(isalpha(letter)) {
+    int pos = hashChar(letter);
+    if(!node->child[pos]) {
+      Trie newNode = startNode();
+      if(newNode)
+        node->child[pos] = newNode;
+      else return 0;
+    }
+    return insereTrie(&(node->child[pos]), str + 1);
+  } else 
+    return insereTrie(tr, str + 1); //if not alpha, ignore
 }
 
-int removeTrie(Trie* tr, char *str) {
+int buscaTrie(Trie *tr, char *str) {
+  if (!tr || !str)
+    return 0;
+  Trie node = *tr;
+  char letter = *str;
 
+  if (letter == '\0' && node->end) 
+    return 1;
+  
+
+  if(isalpha(letter)) { 
+    int pos = hashChar(letter);
+    if (!node->child[pos])
+      return 0;
+    return buscaTrie(&(node->child[pos]), str + 1);
+  } else 
+    return buscaTrie(tr, str + 1);
+}
+
+int removeTrie(Trie *tr, char *str) {
+
+}
+
+void imprimeSubTrie(Trie tr, char *word, int tam) {
+
+  if(!tr) return;
+
+  if(tr->end) {
+    word[tam] = '\0';
+    printf("%s\n", word);
+  }
+
+  for(size_t i = 0; i < MAX_CHARS; i++) {
+    if(tr->child[i]) {
+      word[tam] = getChar(i);
+      imprimeSubTrie(tr->child[i], word, tam + 1);
+    }
+  }
 }
 
 void imprimeTrie(Trie *tr) {
-  
+  if (!tr) return;
+  char word[100]; 
+  imprimeSubTrie(*tr, word, 0);
 }
 
+Trie arvorePrefixo(Trie tr, char *prefixo) {
+  char letter = *prefixo;
+  if (letter == '\0') 
+    return tr;
 
-void autocompletarTrie(Trie* tr, char *prefixo) {
+  if(isalpha(letter)) {
+    int pos = hashChar(letter);
+    if(tr->child[pos]) {
+      return arvorePrefixo(tr->child[pos], prefixo + 1);
+    } else return NULL;
+  } else return arvorePrefixo(tr, prefixo + 1);
+}
 
+void autocompletarTrie(Trie *tr, char *prefixo) {
+  if(!tr || !prefixo) return;
+  imprimeSubTrie(arvorePrefixo(*tr, prefixo), prefixo, strlen(prefixo));
 }
