018327225d0e2a22731bbdac543872d5900f19cb
guilherme.092011 <guilherme.092011@hotmail.com>
2021-06-04 17:23:50 -0300

    tmj fml

--- src/app.c
+++ src/app.c
@@ -14,102 +14,82 @@ int main() {
     printf("Problemas para criar a arvore\n");
     return 0;
   }
-
   FILE *arq = fopen("./test/words.txt", "r");
   if (arq == NULL) {
     printf("Erro a abrir o arquivo\n");
     return 0;
   }
-  //Insercao das palavras----------------------------------------------------------------
 
+  //Insercao das palavras----------------------------------------------------------------
   char palavra[50];
   while (1) {
     if (feof(arq))
       break;
     fscanf(arq, "%s", palavra);
     insereTrie(trie, palavra);
-    // if (insereTrie(trie, palavra))
-    //   printf("Sucesso ao inserir %s\n", palavra);
-    // else {
-    //   printf("Falha ao inserir %s\n", palavra);
-    //   break;
-    // }
   }
   //-------------------------------------------------------------------------------------
-  
-  char op;
-  while (1) {
-    printf("1-Inserir palavra\n2-Buscar palavra\n3-Remover palavra\n4-Imprimir Trie\n5-ORA ORA ORA\n6-Imprimir prefixo");
-    printf("\n7-Finalizar execucao\nQual opcao?: ");
-    op = getchar();
+  int op;
+  while(1) {
+    printf("1-Inserir palavra\n2-Buscar palavra\n3-Remover palavra\n4-Imprimir Trie\n5-Imprimir prefixo");
+    printf("\n6-Finalizar execucao\nQual opcao?: ");
+    scanf("%i", &op);
     switch (op) {
       //-------------------------------------------------------------------------------------
-    case '1':
-      printf("Qual palavra deseja inserir?\n");
-      scanf("%s", palavra);
-      if (insereTrie(trie, palavra)) {
-        printf("Sucesso na insercao de %s\n", palavra);
-      } else {
-        printf("Falha na insercao %s\n", palavra);
-      }
-      break;
+      case 1:
+        printf("Qual palavra deseja inserir?\n");
+        scanf("%s", palavra);
+        setbuf(stdin, NULL); 
+        if (insereTrie(trie, palavra)) {
+          printf("Sucesso na insercao de %s\n", palavra);
+        } else {
+          printf("Falha na insercao %s\n", palavra);
+        }
+        break;
       //-------------------------------------------------------------------------------------
-    case '2':
-      printf("Qual palavra deseja buscar?\n");
-      scanf("%s", palavra);
-      if (buscaTrie(trie, palavra)) {
-        printf("Esta palavra esta na arvore\n");
-      } else {
-        printf("Esta palavra nao esta na arvore\n");
-      }
-      break;
+      case 2:
+        printf("Qual palavra deseja buscar?\n");
+        scanf("%s", palavra);
+        setbuf(stdin, NULL); 
+        if (buscaTrie(trie, palavra)) {
+          printf("Esta palavra esta na arvore\n");
+        } else {
+          printf("Esta palavra nao esta na arvore\n");
+        }
+        break;
       //-------------------------------------------------------------------------------------
-    case '3':
-      printf("Qual palavra deseja remover?\n");
-      scanf("%s",palavra);
-      if(removeTrie(trie, palavra))
-        printf("Sucesso na remocao\n");
-      else
-        printf("Falha na remocao\n");  
-      break;
+      case 3:
+        printf("Qual palavra deseja remover?\n");
+        scanf("%s",palavra);
+        setbuf(stdin, NULL); 
+        if(removeTrie(trie, palavra))
+          printf("Sucesso na remocao\n");
+        else
+          printf("Falha na remocao\n");  
+        break;
       //-------------------------------------------------------------------------------------
-    case '4':
-      printf("----------PRINTANDO O TRIE FILHO EXQUECE----------\n");
-      imprimeTrie(trie);
-      break;
+      case 4:
+        imprimeTrie(trie);
+        break;
       //-------------------------------------------------------------------------------------
-    case '5': 
-        printf("⣿⣿⣿⣿⣿⣿⣿⡿⡛⠟⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
-        printf("⣿⣿⣿⣿⣿⣿⠿⠨⡀⠄⠄⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
-        printf("⣿⣿⣿⣿⠿⢁⠼⠊⣱⡃⠄⠈⠹⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
-        printf("⣿⣿⡿⠛⡧⠁⡴⣦⣔⣶⣄⢠⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣤⠭⠏⠙⢿⣿\n");
-        printf("⣿⡧⠠⠠⢠⣾⣾⣟⠝⠉⠉⠻⡒⡂⠄⠙⠻⣿⣿⣿⣿⣿⡪⠘⠄⠉⡄⢹\n");
-        printf("⣿⠃⠁⢐⣷⠉⠿⠐⠑⠠⠠⠄⣈⣿⣄⣱⣠⢻⣿⣿⣿⣿⣯⠷⠈⠉⢀⣾\n");
-        printf("⣿⣴⠤⣬⣭⣴⠂⠇⡔⠚⠍⠄⠄⠁⠘⢿⣷⢈⣿⣿⣿⣿⡧⠂⣠⠄⠸⡜\n");
-        printf("⣿⣇⠄⡙⣿⣷⣭⣷⠃⣠⠄⠄⡄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣧⣁⣿⡄⠼⡿\n");
-        printf("⣿⣷⣥⣴⣿⣿⣿⣿⠷⠲⠄⢠⠄⡆⠄⠄⠄⡨⢿⣿⣿⣿⣿⣿⣎⠐⠄⠈\n");
-        printf("⣿⣿⣿⣿⣿⣿⢟⠕⠁⠈⢠⢃⢸⣿⣿⣶⡘⠑⠄⠸⣿⣿⣿⣿⣿⣦⡀⡉\n");
-        printf("⣿⣿⣿⡟⠄⠄⠄⠄⠄⠋⢀⣼⣿⣿⣿⣿⣿⣿⣿⣶⣦⣀⢟⣻⣿⣿⣿⣿\n");
-        printf("⣿⣿⣿⡆⠆⠄⠠⡀⡀⠄⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
-        printf("⣿⣿⡿⡅⠄⠄⢀⡰⠂⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
-      break;
-    case '6':
-      printf("Qual palavra deseja utilizar?\n");
-      scanf("%s", palavra);
-      autocompletarTrie(trie, palavra);
-      break;
+      case 5:
+        printf("Qual palavra deseja utilizar?\n");
+        scanf("%s", palavra);
+        setbuf(stdin, NULL); 
+        autocompletarTrie(trie, palavra);
+        break;
       //-------------------------------------------------------------------------------------
-    case '7':
-      printf("Liberando a arvore\n");
-      liberaTrie(trie);
-      printf("Fim da execucao\n");
-      return 0;
+      case 6:
+        printf("Liberando a arvore\n");
+        liberaTrie(trie);
+        printf("Fim da execucao\n");
+        return 0;
+      default: 
+        printf("Opcao invalida!\n");
+        break;
     }
-    setbuf(stdin, NULL); 
-    system("pause"); //windows
-    // int c = getchar(); //linux
-    // system("clear"); //linux
-    system("cls"); //windows
+    // system("pause"); //windows
+    // system("cls"); //windows
   }
   liberaTrie(trie);
   free(arq);
